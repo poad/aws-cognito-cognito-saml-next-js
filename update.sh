ed73b04ee6407ab3e934c0005f461fc706612004
Kenji Saito <ken-yo@mbr.nifty.com>
2023-09-03 12:17:37 +0900

    Add apps

---
+++ update.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+CUR=$(pwd)
+
+CURRENT=$(cd "$(dirname "$0")" || exit;pwd)
+echo "${CURRENT}"
+
+cd "${CURRENT}" || exit
+if ! git pull --prune; then
+  cd "${CUR}" || exit
+  exit $result
+fi
+
+cd "${CURRENT}" || exit
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit $result
+fi
+echo ""
+pwd
+if ! pnpm install -r && pnpm up -r && pnpm -r build ; then
+  cd "${CUR}" || exit
+  exit $result
+fi
+
+cd "${CURRENT}" || exit
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit $result
+fi
+
+if ! git pull --prune && git commit -am "Bumps node modules" && git push; then
+  cd "${CUR}" || exit
+  exit $result
+fi
+
+cd "${CUR}" || exit
